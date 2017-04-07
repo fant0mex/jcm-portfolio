74acec8f61a919c0d32bebd707bdf0116a90590b
Joe Hobson <joseph.j.hobson@googlemail.com>
2017-04-07 16:37:16 +0100

    feat: add alias resolver

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './App'
+import Homepage from 'app/pages/landing/Homepage'
 
-render(<App />, document.getElementById('app'))
+render(<Homepage />, document.getElementById('app'))
--- src/scripts/app/pages/landing/HomePage.js
+++ src/scripts/app/pages/landing/HomePage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { StyleSheet, css } from 'aphrodite/no-important'
 
 class App extends Component {
   render () {
     return (
-      <h1 className={css(styles.heading)}>James' Site</h1>
+      <Router>
+        <h1 className={css(styles.heading)}>Home Page</h1>
+      </Router>
     )
   }
 }
