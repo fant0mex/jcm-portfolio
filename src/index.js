f79ef4bfeef8423f1994adba148ea4bb629a772d
Joe Hobson <joseph.j.hobson@googlemail.com>
2017-04-07 17:00:21 +0100

    fix: normalize css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Homepage from 'app/pages/landing/Homepage'
+import 'styles/style.css'
 
 render(<Homepage />, document.getElementById('app'))
