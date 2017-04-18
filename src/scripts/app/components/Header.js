a06d565bdbdc2078f47b7850a274d6c6dd41dbe0
bushers <nbushby01@hotmail.com>
2017-04-18 18:41:20 +0100

    style header

--- src/scripts/app/components/Header.js
+++ src/scripts/app/components/Header.js
@@ -4,30 +4,68 @@ import { StyleSheet, css } from 'aphrodite/no-important'
 import styleVars from 'styles/variables'
 
 const Header = () => (
-  <nav>
-    <ul className={css(styles.list)}>
-      <li><NavLink activeStyle={{color: 'green'}} to='/'>Home</NavLink></li>
-      <li><NavLink activeStyle={{color: 'green'}} to='/reel'>Reel</NavLink></li>
-      <li><NavLink activeStyle={{color: 'green'}} to='/projects'>Projects</NavLink></li>
-      <li><NavLink activeStyle={{color: 'green'}} to='/photography'>Photography</NavLink></li>
-      <li><NavLink activeStyle={{color: 'green'}} to='/blog'>Blog</NavLink></li>
-      <li><NavLink activeStyle={{color: 'green'}} to='/contact'>Contact</NavLink></li>
-    </ul>
-    <hr />
-  </nav>
+  <header className={css(styles.header)}>
+    <div className={css(styles.logo)}>
+      <img src='src/assets/img/jcm-logo.png' />
+    </div>
+    <nav className={css(styles.nav)}>
+      <ul className={css(styles.list)}>
+        <li className={css(styles.listItem)}><NavLink activeStyle={{color: 'green'}} to='/'>Home</NavLink></li>
+        <li className={css(styles.listItem)}><NavLink activeStyle={{color: 'green'}} to='/reel'>Reel</NavLink></li>
+        <li className={css(styles.listItem)}><NavLink activeStyle={{color: 'green'}} to='/projects'>Projects</NavLink></li>
+        <li className={css(styles.listItem)}><NavLink activeStyle={{color: 'green'}} to='/photography'>Photography</NavLink></li>
+        <li className={css(styles.listItem)}><NavLink activeStyle={{color: 'green'}} to='/blog'>Blog</NavLink></li>
+        <li className={css(styles.listItem)}><NavLink activeStyle={{color: 'green'}} to='/contact'>Contact</NavLink></li>
+      </ul>
+    </nav>
+  </header>
 )
 
 export default Header
 
 const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight: 'auto',
+    padding: '0 0.5em',
+    [styleVars.media.lg]: {
+      flexDirection: 'row',
+      alignItems: 'flex-end'
+    }
+  },
+
+  logo: {
+    width: '16em',
+    [styleVars.media.md]: {
+      width: '25em'
+    }
+  },
+
+  nav: {
+    width: '90%',
+    [styleVars.media.lg]: {
+      width: '50%'
+    }
+  },
+
   list: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
-    padding: '1rem 1rem 0',
+    padding: '0 1rem',
     fontFamily: styleVars.font.Palanquin,
+    textTransform: 'lowercase',
+    textAlign: 'center',
+    fontSize: '1.15em',
     [styleVars.media.sm]: {
       flexDirection: 'row'
     }
+  },
+
+  listItem: {
+    margin: '0 1em'
   }
 })
