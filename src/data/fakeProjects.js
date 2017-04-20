d9d66787c8a01868a0f100945819a2f15e01006a
bushers <nbushby01@hotmail.com>
2017-04-20 19:07:32 +0100

    add dummy projects data

---
+++ src/data/fakeProjects.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    featuredImage: {
+      url: 'https://static.pexels.com/photos/356056/pexels-photo-356056.jpeg'
+    },
+    slug: 'fake-post-one',
+    title: 'A Fake Project'
+  },
+  {
+    featuredImage: {
+      url: 'https://static.pexels.com/photos/292627/pexels-photo-292627.jpeg'
+    },
+    slug: 'fake-post-two',
+    title: 'Another Fake Project'
+  },
+  {
+    featuredImage: {
+      url: 'https://static.pexels.com/photos/197526/pexels-photo-197526.jpeg'
+    },
+    slug: 'fake-post-three',
+    title: 'More Lies'
+  }
+]
