46a4c1fea9de746b8fdf26d3d9e881b9b88da9a9
Nick Bushby <nbushby01@hotmail.com>
2017-04-28 21:54:43 +0100

    add nav menu for mobile

--- src/scripts/app/components/Header.js
+++ src/scripts/app/components/Header.js
@@ -4,6 +4,7 @@ import { StyleSheet, css } from 'aphrodite/no-important'
 import Logo from 'assets/img/jcm-logo.png'
 import styleVars from 'styles/variables'
 import MediaQuery from 'react-responsive'
+import { slide as Menu } from 'react-burger-menu'
 
 const Header = () => (
   <header className={css(styles.header)}>
@@ -13,7 +14,17 @@ const Header = () => (
       </NavLink>
     </div>
     <MediaQuery maxWidth={768}>
-      <div>'Test for hamburger menu'</div>
+      <Menu styles={burger} isOpen={false} right>
+        <nav className={css(styles.nav)}>
+          <ul className={css(styles.list)}>
+            <li><NavLink activeClassName={css(styles.active)} className={css(styles.listItem)} to='/reel'>Reel</NavLink></li>
+            <li><NavLink activeClassName={css(styles.active)} className={css(styles.listItem)} to='/projects'>Projects</NavLink></li>
+            <li><NavLink activeClassName={css(styles.active)} className={css(styles.listItem)} to='/photography'>Photography</NavLink></li>
+            <li><NavLink activeClassName={css(styles.active)} className={css(styles.listItem)} to='/blog'>Blog</NavLink></li>
+            <li><NavLink activeClassName={css(styles.active)} className={css(styles.listItem)} to='/contact'>Contact</NavLink></li>
+          </ul>
+        </nav>
+      </Menu>
     </MediaQuery>
     <MediaQuery minWidth={769}>
       <nav className={css(styles.nav)}>
@@ -31,16 +42,59 @@ const Header = () => (
 
 export default Header
 
+const burger = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '30px',
+    height: '22px',
+    right: '26px',
+    top: '26px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '34px',
+    width: '34px',
+    top: '10px',
+    right: '20px'
+  },
+  bmCross: {
+    background: '#bdc3c7',
+    height: '24px'
+  },
+  bmMenuWrap: {
+    top: 0
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '1em 1.5em 0',
+    fontSize: '1.15em',
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    width: '300px'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)',
+    top: 0,
+    right: 0
+  }
+}
+
 const styles = StyleSheet.create({
   header: {
     display: 'flex',
-    flexFlow: 'column nowrap',
+    flexFlow: 'row nowrap',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginRight: 'auto',
     padding: '0 0.5em',
     [styleVars.media.lg]: {
-      flexDirection: 'row',
       alignItems: 'flex-end'
     }
   },
@@ -53,7 +107,6 @@ const styles = StyleSheet.create({
   },
 
   nav: {
-    width: '90%',
     [styleVars.media.lg]: {
       width: '50%'
     }
@@ -68,15 +121,19 @@ const styles = StyleSheet.create({
     textTransform: 'lowercase',
     textAlign: 'center',
     fontSize: '1.2em',
-    [styleVars.media.sm]: {
-      flexDirection: 'row'
+    [styleVars.media.md]: {
+      flexDirection: 'row',
+      paddingBottom: '1em'
     }
   },
 
   listItem: {
     margin: '0 0.5em',
     padding: '0 0.5em',
-    color: '#000'
+    color: '#ddd',
+    [styleVars.media.md]: {
+      color: '#000'
+    }
   },
 
   active: {
