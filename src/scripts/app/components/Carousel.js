6a0dce518b05f9ae97d1659e1ca8c028c35d40f1
Nick Bushby <nbushby01@hotmail.com>
2017-05-03 22:59:18 +0100

    try carousel package

--- src/scripts/app/components/Carousel.js
+++ src/scripts/app/components/Carousel.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import Slider from 'react-slick'
 import { StyleSheet, css } from 'aphrodite/no-important'
+import fakeProjects from 'data/fakeProjects'
 // import styleVars from 'styles/variables'
 
-const Carousel = props => (
-  <div className={css(styles.wrapper)}>
-    Carousel test
-  </div>
-)
+const Carousel = props => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+  return (
+    <div className={css(styles.wrapper)}>
+      <Slider {...settings}>
+        {fakeProjects.map((item, index) => (
+          <div key={index}>
+            <img src={item.featuredImage.url} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  )
+}
 
 export default Carousel
 
 const styles = StyleSheet.create({
   wrapper: {
-    color: 'blue'
+    maxWidth: '1400px'
   }
 })
