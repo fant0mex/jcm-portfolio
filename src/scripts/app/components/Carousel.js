5530af70b580727152a9812c21d6515f88d58963
Nick Bushby <nbushby01@hotmail.com>
2017-05-10 17:37:27 +0100

    add react-slick carousel

--- src/scripts/app/components/Carousel.js
+++ src/scripts/app/components/Carousel.js
@@ -1,19 +1,36 @@
 import React from 'react'
+import Slider from 'react-slick'
 import { StyleSheet, css } from 'aphrodite/no-important'
+import fakeProjects from 'data/fakeProjects'
 
 const Carousel = props => {
   return (
     <div className={css(styles.wrapper)}>
-      <img src={props.img} />
+      <Slider {...settings}>
+        {fakeProjects.map(item => (
+          <img key={item.slug} src={item.featuredImage.url} />
+        ))}
+      </Slider>
     </div>
   )
 }
 
 export default Carousel
 
+const settings = {
+  arrows: false,
+  autoplay: true,
+  dots: false,
+  fade: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
 const styles = StyleSheet.create({
   wrapper: {
     maxWidth: '1200px',
-    margin: 'auto'
+    margin: 'auto',
+    overflow: 'hidden'
   }
 })
--- src/scripts/app/components/Home.js
+++ src/scripts/app/components/Home.js
@@ -2,35 +2,12 @@ import React, { Component } from 'react'
 import Carousel from 'app/components/Carousel'
 import SocialIcons from 'app/components/SocialIcons'
 import { StyleSheet, css } from 'aphrodite/no-important'
-import fakeProjects from 'data/fakeProjects'
-// import styleVars from 'styles/variables'
 
 class Home extends Component {
-  state = {
-    imgShown: fakeProjects[0].featuredImage.url
-  }
-
-  componentDidMount () {
-    const carouselImgTimer = setInterval(this.changeCarouselImg, 3000)
-    this.setState({ carouselImgTimer })
-  }
-
-  componentWillUnmount () {
-    clearInterval(this.state.carouselImgTimer)
-  }
-
-  changeCarouselImg = () => {
-    this.setState({
-      imgShown: fakeProjects[i].featuredImage.url
-    })
-    i += 1
-    if (i === fakeProjects.length) { i = 0 }
-  }
-
   render () {
     return (
       <div className={css(styles.wrapper)}>
-        <Carousel onload={this.carouselImgTimer} img={this.state.imgShown} />
+        <Carousel />
         <SocialIcons />
       </div>
     )
@@ -39,7 +16,6 @@ class Home extends Component {
 
 export default Home
 
-let i = 1
 const styles = StyleSheet.create({
   wrapper: {
     padding: '1em',
