b78eba855464a1bca4a72d4e0f34126b3942c4e1
Nick Bushby <nbushby01@hotmail.com>
2017-05-11 19:04:31 +0100

    redo carousel

--- src/scripts/app/components/Carousel.js
+++ src/scripts/app/components/Carousel.js
@@ -1,36 +1,60 @@
-import React from 'react'
-import Slider from 'react-slick'
+import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite/no-important'
 import fakeProjects from 'data/fakeProjects'
 
-const Carousel = props => {
-  return (
-    <div className={css(styles.wrapper)}>
-      <Slider {...settings}>
-        {fakeProjects.map(item => (
-          <img key={item.slug} src={item.featuredImage.url} />
+class Carousel extends Component {
+  state = {
+    currentImg: 0,
+    arrLength: fakeProjects.length
+  }
+
+  componentDidMount () {
+    setInterval(this.nextSlide, 3000)
+  }
+
+  nextSlide = () => {
+    this.state.currentImg < this.state.arrLength
+    ? this.setState({ currentImg: this.state.currentImg + 1 })
+    : this.setState({ currentImg: 0 })
+  }
+
+  render () {
+    return (
+      <div className={css(styles.wrapper)}>
+        {fakeProjects.map((item, i) => (
+          <img key={item.slug}
+            style={i === this.state.currentImg ? show : null}
+            className={css(styles.slide)}
+            src={item.featuredImage.url}
+          />
         ))}
-      </Slider>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default Carousel
 
-const settings = {
-  arrows: false,
-  autoplay: true,
-  dots: false,
-  fade: true,
-  autoplaySpeed: 3000,
-  slidesToShow: 1,
-  slidesToScroll: 1
+const show = {
+  opacity: '1',
+  zIndex: '2'
 }
 
 const styles = StyleSheet.create({
   wrapper: {
     maxWidth: '1200px',
+    minHeight: '30em',
     margin: 'auto',
-    overflow: 'hidden'
+    position: 'relative'
+  },
+
+  slide: {
+    position: 'absolute',
+    left: '0',
+    top: '0',
+    width: '100%',
+    opacity: '0',
+    zIndex: '1',
+    transition: 'opacity 0.5s'
   }
 })
