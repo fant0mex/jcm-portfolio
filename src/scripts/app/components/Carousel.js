3513002ff1ecc652c763c5c38e60fe9aa0855a10
fant0mex <joseph.j.hobson@googlemail.com>
2017-05-19 11:24:57 +0100

    fix: carousel update

--- src/scripts/app/components/Carousel.js
+++ src/scripts/app/components/Carousel.js
@@ -1,75 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { StyleSheet, css } from 'aphrodite/no-important'
-import fakeProjects from 'data/fakeProjects'
-import classNames from 'classnames'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import 'styles/carousel.css'
 
-class Carousel extends Component {
-  state = {
-    currentImg: 0,
-    arrLength: fakeProjects.length,
-    cssClasses: classNames(css(styles.slide), css(styles.show))
-  }
-
-  componentDidMount () {
-    const slideTimer = setInterval(this.nextSlide, 3000)
-    this.setState({ slideTimer })
-  }
-
-  componentWillUnmount () {
-    clearInterval(this.state.slideTimer)
-  }
-
-  addTransition = () => {
-    setTimeout(() => {
-      this.setState({
-        cssClasses: classNames(css(styles.slide), css(styles.show))
-      })
-    }, 20)
-  }
-
-  nextSlide = () => {
-    this.setState({ cssClasses: classNames(css(styles.slide)) })
-    this.state.currentImg < this.state.arrLength - 1
-    ? (this.setState({ currentImg: this.state.currentImg + 1 }), this.addTransition())
-    : (this.setState({ currentImg: 0 }), this.addTransition())
-  }
-
-  render () {
-    return (
-      <div className={css(styles.wrapper)}>
-        {fakeProjects.map((item, i) => (
-          <img
-            key={item.slug}
-            style={i === this.state.currentImg ? show : null}
-            className={this.state.cssClasses}
-            src={item.featuredImage.url}
-          />
-        ))}
-      </div>
-    )
-  }
-}
+const Carousel = props => (
+  <div className={css(styles.wrapper)}>
+    <CSSTransitionGroup
+      transitionName='carousel'
+      transitionEnterTimeout={2900}
+      transitionLeaveTimeout={2900}>
+      <img
+        className={css(styles.img)}
+        style={{}}
+        src={props.featuredImage}
+        key={props.featuredImage}
+      />
+    </CSSTransitionGroup>
+  </div>
+)
 
 export default Carousel
 
-const show = {
-  display: 'block'
-}
-
 const styles = StyleSheet.create({
   wrapper: {
-    maxWidth: '1000px',
-    margin: 'auto'
+    position: 'relative'
   },
-
-  slide: {
-    display: 'none',
-    width: '100%',
-    opacity: '0',
-    transition: 'opacity 0.5s'
-  },
-
-  show: {
-    opacity: '1'
+  img: {
+    position: 'absolute',
+    top: 0,
+    left: 'calc(50% - 400px)',
+    width: 800,
+    height: 600
   }
 })
--- src/scripts/app/components/Home.js
+++ src/scripts/app/components/Home.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Carousel from 'app/components/Carousel'
-import SocialIcons from 'app/components/SocialIcons'
 import { StyleSheet, css } from 'aphrodite/no-important'
+import fakeProjects from 'data/fakeProjects'
 
-const Home = props => (
-  <div className={css(styles.wrapper)}>
-    <Carousel />
-    <SocialIcons />
-  </div>
-)
+class Home extends Component {
+  state = {
+    currentImg: 0
+  }
+
+  componentDidMount () {
+    setInterval(this.incrementSlider, 6000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.incrementSlider)
+  }
+
+  incrementSlider = () => {
+    const { currentImg } = this.state
+    currentImg < fakeProjects.length - 1 ? this.setState({ currentImg: currentImg + 1 }) : this.setState({ currentImg: 0 })
+  }
+
+  render () {
+    const { currentImg } = this.state
+    return (
+      <div className={css(styles.wrapper)}>
+        <Carousel featuredImage={fakeProjects[currentImg].featuredImage.url} />
+      </div>
+    )
+  }
+}
 
 export default Home
 
 const styles = StyleSheet.create({
   wrapper: {
-    padding: '1em',
-    color: 'red'
+    padding: '1em'
   }
 })
